20ca1f1bd0df8ac43682c872ab72401509821086
Yashveer Beniwal <yashveer.beniwal@climaxmedia.com>
2017-02-06 00:54:00 -0500

    first python

---
+++ isUnique.py
@@ -0,0 +1,14 @@
+
+import sys
+
+def isUnique( string ):
+
+
+    return string
+
+def printMe( string ):
+    print(len(string))
+    return
+"""if __name__ == '__main__':"""
+
+printMe(isUnique('avsscs'))
