fd93f7f29fa05bfb2e75e31124c8f2a524c859c4
yashveer-beniwal <yashveerbeniwal@yashveers-MacBook-Pro.local>
2017-02-13 01:33:08 -0500

    isUnique answer in python

--- isUnique.py
+++ isUnique.py
@@ -1,14 +1,25 @@
-
+"""
+    Implement an algorithm to determine if a string has all unique characters.
+    What if you cannot use additional data structures
+"""
 import sys
+import array
 
 def isUnique( string ):
+    a = array.array('i', (0 for i in range(0, 127)))
+    for value in string:
+        if a[ord(value)] == 1:
+            return False
+        a[ord(value)] = 1
+    return True
 
-
-    return string
-
-def printMe( string ):
-    print(len(string))
+def printMe(string):
+    print('isUnique : ' + str(isUnique(string)))
     return
-"""if __name__ == '__main__':"""
 
-printMe(isUnique('avsscs'))
+"Test Cases"
+printMe('abcdefghsssjkl')
+printMe('1231312412412')
+printMe('absjkls')
+printMe('abcdjkl')
+printMe('abcd')
